f8b4b58f25a2b82d0d4edade017e07357cdf5f99
jpolchlo <jpolchlopek@azavea.com>
2020-03-31 15:14:51 -0400

    Introduce IDWRasterSource and co.

---
+++ pointcloud/src/main/scala/geotrellis/pointcloud/raster/ept/IDWRasterSource.scala
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2020 Azavea
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package geotrellis.pointcloud.raster.ept
+
+import geotrellis.pointcloud.raster.rasterize.triangles.PDALTrianglesRasterizer
+import geotrellis.proj4._
+import geotrellis.raster._
+import geotrellis.raster.interpolation._
+import geotrellis.raster.io.geotiff.OverviewStrategy
+import geotrellis.vector._
+
+import cats.syntax.option._
+import _root_.io.circe.syntax._
+import _root_.io.pdal.pipeline._
+import org.log4s._
+import spire.syntax.cfor._
+
+import scala.collection.JavaConverters._
+
+/**
+  * [[DEMRasterSource]] doesn't use [[OverviewStrategy]].
+  * At this point, it relies on the EPTReader logic:
+  * https://github.com/PDAL/PDAL/blob/2.1.0/io/EptReader.cpp#L293-L318
+  */
+case class IDWRasterSource(
+  path: EPTPath,
+  resampleTarget: ResampleTarget = DefaultTarget,
+  sourceMetadata: Option[EPTMetadata] = None,
+  threads: Option[Int] = None,
+  targetCellType: Option[TargetCellType] = None
+) extends RasterSource {
+  @transient private[this] lazy val logger = getLogger
+
+  lazy val metadata: EPTMetadata = sourceMetadata.getOrElse(EPTMetadata(path.value))
+
+  def attributes: Map[String, String] = metadata.attributes
+  def attributesForBand(band: Int): Map[String, String] = metadata.attributesForBand(band)
+  def bandCount: Int = metadata.bandCount
+  def cellType: CellType = metadata.cellType
+  def crs: CRS = metadata.crs
+  def gridExtent: GridExtent[Long] = metadata.gridExtent
+  def name: SourceName = metadata.name
+  def resolutions: List[CellSize] = metadata.resolutions
+
+  def reprojection(targetCRS: CRS, resampleTarget: ResampleTarget, method: ResampleMethod, strategy: OverviewStrategy): IDWReprojectRasterSource =
+    IDWReprojectRasterSource(path.value, targetCRS, resampleTarget, sourceMetadata = metadata.some, threads = threads, method, targetCellType = targetCellType)
+
+  def resample(resampleTarget: ResampleTarget, method: ResampleMethod, strategy: OverviewStrategy): IDWResampleRasterSource =
+    IDWResampleRasterSource(path.value, resampleTarget, metadata.some, threads, method, targetCellType)
+
+  def read(bounds: GridBounds[Long], bands: Seq[Int]): Option[Raster[MultibandTile]] = {
+    val targetRegion = gridExtent.extentFor(bounds, clamp = false)
+    val Extent(exmin, eymin, exmax, eymax) = targetRegion.extent
+
+    val expression = ReadEpt(
+      filename   = path.value,
+      resolution = gridExtent.cellSize.resolution.some,
+      bounds     = s"([$exmin, $eymin], [$exmax, $eymax])".some,
+      threads    = threads
+    )
+
+    logger.debug(expression.asJson.spaces4)
+
+    val pipeline = expression toPipeline
+
+    try {
+      if(pipeline.validate()) {
+        pipeline.execute
+
+        val pointViews = pipeline.getPointViews().asScala.toList
+        assert(pointViews.length == 1, "Triangulation pipeline should have single resulting point view")
+
+        pointViews.headOption.map { pv =>
+          val pc = pv.getPointCloud
+          val features = Array.ofDim[PointFeature[Double]](pc.length)
+          cfor(0)(_ < pc.length, _ + 1){ i =>
+            features(i) = PointFeature[Double](Point(pc.getX(i), pc.getY(i)), pc.getZ(i))
+          }
+
+          val re = RasterExtent(
+            targetRegion,
+            bounds.width.toInt,
+            bounds.height.toInt
+          )
+
+          features
+            .toTraversable
+            .inverseDistanceWeighted(
+              re,
+              InverseDistanceWeighted.Options(
+                re.cellwidth,
+                re.cellheight,
+                0.0,  // rotation
+                3.0,  // weighting power
+                0.0,  // smoothing factor
+                re.cellSize.resolution/2, //equal weight radius
+                cellType
+              ))
+            .mapTile(MultibandTile(_))
+        }
+      } else None
+    } finally pipeline.close()
+  }
+
+  def read(extent: Extent, bands: Seq[Int]): Option[Raster[MultibandTile]] = {
+    val bounds = gridExtent.gridBoundsFor(extent.buffer(- cellSize.width / 2, - cellSize.height / 2), clamp = false)
+    read(bounds, bands)
+  }
+
+  def convert(targetCellType: TargetCellType): RasterSource =
+    throw new UnsupportedOperationException("DEM height fields may only be of floating point type")
+}
---
+++ pointcloud/src/main/scala/geotrellis/pointcloud/raster/ept/IDWResampleRasterSource.scala
@@ -0,0 +1,166 @@
+/*
+ * Copyright 2020 Azavea
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package geotrellis.pointcloud.raster.ept
+
+import geotrellis.pointcloud.raster.rasterize.triangles.PDALTrianglesRasterizer
+import geotrellis.proj4._
+import geotrellis.raster._
+import geotrellis.raster.interpolation._
+import geotrellis.raster.io.geotiff.OverviewStrategy
+import geotrellis.raster.reproject.{Reproject, ReprojectRasterExtent}
+import geotrellis.raster.resample.NearestNeighbor
+import geotrellis.vector._
+
+import cats.syntax.option._
+import _root_.io.circe.syntax._
+import _root_.io.pdal.pipeline._
+import org.log4s._
+import spire.syntax.cfor._
+
+import scala.collection.JavaConverters._
+
+/**
+  * [[IDWResampleRasterSource]] doesn't use [[OverviewStrategy]].
+  * At this point, it relies on the EPTReader logic:
+  * https://github.com/PDAL/PDAL/blob/2.1.0/io/EptReader.cpp#L293-L318
+  */
+case class IDWResampleRasterSource(
+  path: EPTPath,
+  resampleTarget: ResampleTarget = DefaultTarget,
+  sourceMetadata: Option[EPTMetadata] = None,
+  threads: Option[Int] = None,
+  resampleMethod: ResampleMethod = NearestNeighbor,
+  targetCellType: Option[TargetCellType] = None
+) extends RasterSource {
+  @transient private[this] lazy val logger = getLogger
+
+  lazy val baseMetadata: EPTMetadata = sourceMetadata.getOrElse(EPTMetadata(path.value))
+  lazy val metadata: EPTMetadata = baseMetadata.copy(gridExtent = gridExtent)
+
+  protected lazy val baseCRS: CRS = baseMetadata.crs
+  protected lazy val baseGridExtent: GridExtent[Long] = baseMetadata.gridExtent
+
+  // TODO: remove transient notation with Proj4 1.1 release
+  @transient protected lazy val transform = Transform(baseCRS, crs)
+  @transient protected lazy val backTransform = Transform(crs, baseCRS)
+
+  def attributes: Map[String, String] = metadata.attributes
+  def attributesForBand(band: Int): Map[String, String] = metadata.attributesForBand(band)
+  def bandCount: Int = baseMetadata.bandCount
+  def cellType: CellType = baseMetadata.cellType
+  def crs: CRS = baseMetadata.crs
+  def name: SourceName = baseMetadata.name
+  def resolutions: List[CellSize] = baseMetadata.resolutions
+
+  lazy val gridExtent: GridExtent[Long] = resampleTarget(baseMetadata.gridExtent)
+
+  def reprojection(targetCRS: CRS, resampleTarget: ResampleTarget, method: ResampleMethod, strategy: OverviewStrategy): IDWReprojectRasterSource = {
+    new IDWReprojectRasterSource(path.value, targetCRS, resampleTarget, baseMetadata.some, threads, method, targetCellType = targetCellType) {
+      override lazy val gridExtent: GridExtent[Long] = {
+        val reprojectedRasterExtent =
+          ReprojectRasterExtent(
+            baseGridExtent,
+            transform,
+            Reproject.Options.DEFAULT.copy(method = resampleMethod, errorThreshold = errorThreshold)
+          )
+
+        resampleTarget match {
+          case targetRegion: TargetRegion => targetRegion.region.toGridType[Long]
+          case targetAlignment: TargetAlignment => targetAlignment(reprojectedRasterExtent)
+          case targetDimensions: TargetDimensions => targetDimensions(reprojectedRasterExtent)
+          case targetCellSize: TargetCellSize => targetCellSize(reprojectedRasterExtent)
+          case _ => reprojectedRasterExtent
+        }
+      }
+    }
+  }
+
+  def resample(resampleTarget: ResampleTarget, method: ResampleMethod, strategy: OverviewStrategy): IDWResampleRasterSource =
+    IDWResampleRasterSource(path.value, resampleTarget, baseMetadata.some, threads, method, targetCellType)
+
+  def read(bounds: GridBounds[Long], bands: Seq[Int]): Option[Raster[MultibandTile]] = {
+    bounds.intersection(dimensions).flatMap { targetPixelBounds =>
+      val targetRegion = RasterExtent(
+        extent = gridExtent.extentFor(targetPixelBounds, clamp = true),
+        cols = targetPixelBounds.width.toInt,
+        rows = targetPixelBounds.height.toInt
+      )
+
+      val Extent(exmin, eymin, exmax, eymax) = targetRegion.extent
+
+      val expression = ReadEpt(
+        filename   = path.value,
+        resolution = targetRegion.cellSize.resolution.some,
+        bounds     = s"([$exmin, $eymin], [$exmax, $eymax])".some,
+        threads    = threads
+      )
+
+      logger.debug(expression.asJson.spaces4)
+
+      val pipeline = expression toPipeline
+
+      try {
+        if(pipeline.validate()) {
+          pipeline.execute
+
+          val pointViews = pipeline.getPointViews().asScala.toList
+          assert(pointViews.length == 1, "Triangulation pipeline should have single resulting point view")
+
+          val pv = pointViews.head
+          val raster = {
+            val pc = pv.getPointCloud
+            val features = Array.ofDim[PointFeature[Double]](pc.length)
+            cfor(0)(_ < pc.length, _ + 1){ i =>
+              features(i) = PointFeature[Double](Point(pc.getX(i), pc.getY(i)), pc.getZ(i))
+            }
+
+            val re = RasterExtent(
+              targetRegion.extent,
+              bounds.width.toInt,
+              bounds.height.toInt
+            )
+
+            features
+              .toTraversable
+              .inverseDistanceWeighted(
+                re,
+                InverseDistanceWeighted.Options(
+                  re.cellwidth,
+                  re.cellheight,
+                  0.0,  // rotation
+                  3.0,  // weighting power
+                  0.0,  // smoothing factor
+                  re.cellSize.resolution/2, //equal weight radius
+                  cellType
+                ))
+              .mapTile(MultibandTile(_))
+          }
+
+          convertRaster(raster).some
+        } else None
+      } finally pipeline.close()
+    }
+  }
+
+  def read(extent: Extent, bands: Seq[Int]): Option[Raster[MultibandTile]] = {
+    val bounds = gridExtent.gridBoundsFor(extent.buffer(- cellSize.width / 2, - cellSize.height / 2), clamp = false)
+    read(bounds, bands)
+  }
+
+  def convert(targetCellType: TargetCellType): RasterSource =
+    throw new UnsupportedOperationException("DEM height fields may only be of floating point type")
+}
